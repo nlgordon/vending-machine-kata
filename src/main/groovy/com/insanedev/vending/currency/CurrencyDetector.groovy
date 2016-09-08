c41147bd9ed8c0af70859f81dca5f5e853779992
Nate Gordon <nlgordon@gmail.com>
2016-09-07 22:45:00 -0500

    Ignoring pennies Adding a default tolerance that can be configured

--- src/main/groovy/com/insanedev/vending/currency/CurrencyDetector.groovy
+++ src/main/groovy/com/insanedev/vending/currency/CurrencyDetector.groovy
@@ -2,8 +2,18 @@ package com.insanedev.vending.currency
 
 class CurrencyDetector {
 
+	BigDecimal tolerance = 0.05
+
 	CoinType analyzeCoin(BigDecimal weight, BigDecimal diameter, BigDecimal thickness) {
-		CoinType ret = CoinType.find {it.weight == weight && it.diameter == diameter && it.thickness == thickness}
+		CoinType ret = CoinType.find {
+			withinTolerance(it.weight, weight) &&
+				withinTolerance(it.diameter, diameter) &&
+				withinTolerance(it.thickness, thickness)
+		}
 		return ret
+	}
+
+	boolean withinTolerance(BigDecimal desired, BigDecimal measured) {
+		return ((desired * (1 - tolerance) <= measured) && (desired * (1 + tolerance) >= measured))
 	}
 }
--- src/test/groovy/com/insanedev/vending/currency/CurrencyDetectorTest.groovy
+++ src/test/groovy/com/insanedev/vending/currency/CurrencyDetectorTest.groovy
@@ -37,4 +37,35 @@ class CurrencyDetectorTest extends Specification {
 		then:
 		assert type == CoinType.NICKEL
 	}
+
+	def "analyzeCoin when a penny is inserted it is not recognized"() {
+		when:
+		CoinType type = detector.analyzeCoin(2.500, 19.05, 1.52)
+
+		then:
+		assert type == null
+	}
+
+	def "analyzeCoin has a default 5% tolerance on the specifications of quarters"() {
+		when:
+		CoinType closeType = detector.analyzeCoin(CoinType.QUARTER.weight * 0.95, CoinType.QUARTER.diameter * 0.95, CoinType.QUARTER.thickness * 0.95)
+		CoinType tooFarType = detector.analyzeCoin(CoinType.QUARTER.weight * 0.94, CoinType.QUARTER.diameter * 0.94, CoinType.QUARTER.thickness * 0.94)
+
+		then:
+		assert closeType == CoinType.QUARTER
+		assert tooFarType == null
+	}
+
+	def "analyzeCoin can be configured to have a 0% tolerance on the specifications of quarters"() {
+		setup:
+		detector.setTolerance(0)
+
+		when:
+		CoinType closeType = detector.analyzeCoin(CoinType.QUARTER.weight, CoinType.QUARTER.diameter, CoinType.QUARTER.thickness)
+		CoinType tooFarType = detector.analyzeCoin(CoinType.QUARTER.weight * 0.99, CoinType.QUARTER.diameter * 0.99, CoinType.QUARTER.thickness * 0.99)
+
+		then:
+		assert closeType == CoinType.QUARTER
+		assert tooFarType == null
+	}
 }
